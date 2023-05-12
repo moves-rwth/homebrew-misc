e261038b1764e7b802f7e88a7705590b08c180af
Alex Bork <alexander.bork@cs.rwth-aachen.de>
2023-05-12 15:41:52 +0200

    Removed options not used for Storm

--- Formula/carl-storm.rb
+++ Formula/carl-storm.rb
@@ -7,29 +7,25 @@ class CarlStorm < Formula
 
   head "https://github.com/moves-rwth/carl-storm.git", using: :git
 
-  option "with-thread-safe", "Build with thread-safe support"
-
   depends_on "boost"
   depends_on "cmake"
   depends_on "eigen"
   depends_on "gmp"
-  on_intel do
+ on_intel do
     depends_on "cln" => :optional
     depends_on "ginac" => :optional
   end
-  depends_on "moves-rwth/misc/cocoalib" => :optional
 
   def install
     args = %w[
       -DEXPORT_TO_CMAKE=OFF
       -DCMAKE_BUILD_TYPE=RELEASE
       -DEXCLUDE_TESTS_FROM_ALL=ON
+      -DTHREAD_SAFE=ON
     ]
-    args << "-DTHREAD_SAFE=ON" if build.with?("thread-safe")
     args << "-DUSE_CLN_NUMBERS=ON" if build.with?("cln") && Hardware::CPU.intel?
     args << "-DUSE_GINAC=ON" if build.with?("ginac") && Hardware::CPU.intel?
     args << "-DUSE_CLN_NUMBERS=OFF -DUSE_GINAC=OFF" if Hardware::CPU.arm?
-    args << "-DUSE_COCOA=ON" if build.with?("cocoalib")
 
     mktemp do
       system "cmake", buildpath, *(std_cmake_args + args)
